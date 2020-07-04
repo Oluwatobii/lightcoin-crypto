8231a13da475473cd861739913580d7340959ed2
Oluwatobii <bellotobi10@gmail.com>
2020-07-04 16:56:30 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let balance = 500.00;
+//Account Class
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+}
 
-class Withdrawal {
-
-  constructor(amount) {
+//Transaction Class
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
+// Withdrawal Class
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
-
+// Deposit Class
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("Otbi");
 
-t1 = new Withdrawal(50.25);
+console.log("Starting Balance: ", myAccount.balance);
+
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(10.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(20.0, myAccount);
+t3.commit();
+
+console.log("Ending Balance:", myAccount.balance);
